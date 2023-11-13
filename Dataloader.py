018bfc014f10fa4c80025a36b11139f13ef3747b
Amany Omar <amany.omar@stud.th-deg.de>
2023-11-13 18:15:30 +0100

    in progress - 13.11.2023

--- Dataloader.py
+++ Dataloader.py
@@ -81,15 +81,16 @@ def dataloader(Dataset_name):
 def read_image(img_path):
     """Keep reading image until succeed.
     This can avoid IOError incurred by heavy IO process."""
-    got_img = False
-    while not got_img:
-        try:
-            img = Image.open(img_path).convert('RGB')
-            got_img = True
-        except IOError:
-            print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
-            pass
-    return img
+    # got_img = False
+    # while not got_img:
+    #     try:
+    #         img = Image.open(img_path).convert('RGB')
+    #         got_img = True
+    #     except IOError:
+    #         print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+    #         pass
+    # return img
+    
 
 class VideoDataset(Dataset):
     """Video Person ReID Dataset.
@@ -141,7 +142,7 @@ class VideoDataset(Dataset):
             targt_cam=[]
             for index in indices:
                 index=int(index)
-                img_path = img_paths   # img_paths[index] as it has the image path without being in a list in the tracklet
+                img_path = img_paths[index]   # img_paths[index] as it has the image path without being in a list in the tracklet
                 img = read_image(img_path)
                 if self.transform is not None:
                     img = self.transform(img)
--- Datasets/FRIDA_dataset.py
+++ Datasets/FRIDA_dataset.py
@@ -66,6 +66,53 @@ class FRIDA(object):
         if not os.path.exists(self.data_dir):
             raise RuntimeError("'{}' is not available".format(self.data_dir))
 
+    # def _process_data(self, dirnames, min_seq_len=0, num_train_ids=10):
+    #     tracklets_train = []
+    #     tracklets_test = []
+    #     num_imgs_per_tracklet_train = []
+    #     num_imgs_per_tracklet_test = []
+    #     pid_container = list(range(20))  # Assuming 20 persons in total
+
+    #     # Randomly shuffle the list of person IDs
+    #     random.shuffle(pid_container)
+
+    #     # Select the first num_train_ids for training, and the rest for testing
+    #     selected_persons_train = pid_container[:num_train_ids]
+    #     selected_persons_test = pid_container[num_train_ids:]
+
+    #     for segment in dirnames:
+    #         for camera in self.cameras:
+    #             json_file = os.path.join(self.data_dir, 'Annotations', segment, camera, 'data2.json')
+    #             with open(json_file, 'r') as f:
+    #                 data = json.load(f)
+
+    #             for person_info in data:
+    #                 img_id = person_info['image_id']
+    #                 pid = person_info['person_id']
+    #                 person_id = f'person_{str(pid).zfill(2)}'  # Convert integer ID to zero-padded string
+                        
+    #                 img_path = os.path.join(self.data_dir, 'BBs', segment, img_id, camera, f'{person_id}.jpg')
+
+    #                 tracklet = ((img_path,), pid, self.cameras.index(camera))  # Update tracklet structure
+    #                 if pid in selected_persons_train:
+    #                     tracklets_train.append(tracklet)
+    #                     num_imgs_per_tracklet_train.append(len(tracklet[0]))  # Access the tuple of image paths
+    #                 elif pid in selected_persons_test:
+    #                     tracklets_test.append(tracklet)
+    #                     num_imgs_per_tracklet_test.append(len(tracklet[0]))  # Access the tuple of image paths
+
+    #     # Filter out tracklets with fewer images than the specified minimum sequence length
+    #     tracklets_train = [tracklet for tracklet in tracklets_train if len(tracklet) >= min_seq_len]
+    #     tracklets_test = [tracklet for tracklet in tracklets_test if len(tracklet) >= min_seq_len]
+
+    #     num_train_tracklets = len(tracklets_train)
+    #     num_test_tracklets = len(tracklets_test)
+    #     num_train_pids = len(selected_persons_train)
+    #     num_test_pids = len(selected_persons_test)
+
+    #     return tracklets_train, tracklets_test, num_train_tracklets, num_test_tracklets, \
+    #             num_train_pids, num_test_pids, num_imgs_per_tracklet_train, num_imgs_per_tracklet_test
+
     def _process_data(self, dirnames, min_seq_len=0, num_train_ids=10):
         tracklets_train = []
         tracklets_test = []
@@ -78,43 +125,38 @@ class FRIDA(object):
 
         # Select the first num_train_ids for training, and the rest for testing
         selected_persons_train = pid_container[:num_train_ids]
-        selected_persons_test = pid_container[num_train_ids:]
-
+        selected_persons_test = pid_container[num_train_ids:] 
         for segment in dirnames:
             for camera in self.cameras:
                 json_file = os.path.join(self.data_dir, 'Annotations', segment, camera, 'data2.json')
                 with open(json_file, 'r') as f:
                     data = json.load(f)
 
-                # for person_info in data:
-                #     img_id = person_info['image_id']
-                #     pid = person_info['person_id']
-                #     person_id = f'person_{str(pid).zfill(2)}'  # Convert integer ID to zero-padded string
-                #     img_path = os.path.join(self.data_dir, 'BBs', segment, img_id, camera, f'{person_id}.jpg')
-
-                #     if pid in selected_persons_train:
-                #         tracklets_train.append((img_path, person_id, self.cameras.index(camera)))
-                #         num_imgs_per_tracklet_train.append(1)
-                #     elif pid in selected_persons_test:
-                #         tracklets_test.append((img_path, person_id, self.cameras.index(camera)))
-                #         num_imgs_per_tracklet_test.append(1)
                 for person_info in data:
                     img_id = person_info['image_id']
                     pid = person_info['person_id']
                     person_id = f'person_{str(pid).zfill(2)}'  # Convert integer ID to zero-padded string
-                    img_path = os.path.join(self.data_dir, 'BBs', segment, img_id, camera, f'{person_id}.jpg')
 
-                    tracklet = (img_path, )
+                    # Create a list to store image paths for the current tracklet
+                    tracklet_images = []
 
+                    for camera_idx in range(len(self.cameras)):
+                        camera_name = f'Camera_{camera_idx + 1}'
+                        img_path = os.path.join(self.data_dir, 'BBs', segment, img_id, camera_name, f'{person_id}.jpg')
+                        tracklet_images.append(img_path)
+
+                    tracklet = (tuple(tracklet_images), pid, self.cameras.index(camera))
+                    
                     if pid in selected_persons_train:
-                        tracklets_train.append((tracklet), pid, self.cameras.index(camera))
-                        num_imgs_per_tracklet_train.append(len(tracklet))
+                        tracklets_train.append(tracklet)
+                        num_imgs_per_tracklet_train.append(len(tracklet_images))  # Tracklet contains all images for the person
                     elif pid in selected_persons_test:
-                        tracklets_test.append((tracklet), pid, self.cameras.index(camera))
-                        num_imgs_per_tracklet_test.append(len(tracklet))
+                        tracklets_test.append(tracklet)
+                        num_imgs_per_tracklet_test.append(len(tracklet_images))  # Tracklet contains all images for the person
+
         # Filter out tracklets with fewer images than the specified minimum sequence length
-        tracklets_train = [tracklet for tracklet in tracklets_train if len(tracklet) >= min_seq_len]
-        tracklets_test = [tracklet for tracklet in tracklets_test if len(tracklet) >= min_seq_len]
+        tracklets_train = [tracklet for tracklet in tracklets_train if len(tracklet[0]) >= min_seq_len]
+        tracklets_test = [tracklet for tracklet in tracklets_test if len(tracklet[0]) >= min_seq_len]
 
         num_train_tracklets = len(tracklets_train)
         num_test_tracklets = len(tracklets_test)
@@ -136,17 +178,15 @@ class FRIDA(object):
         num_query_pids, num_gallery_pids = set(), set()
 
         for tracklet in tracklets:
-            for img_paths, person_id, camera_idx in tracklet:
-                if camera_idx == 0:  # Camera A
-                    #query[person_id][camera_idx] = img_path
-                    query.append(tracklet)
-                    tracklet_query += 1
-                    num_query_pids.add(person_id)
-                else:  # Cameras B and C
-                    #gallery[person_id][camera_idx] = img_path
-                    gallery.append(tracklet)
-                    tracklet_gallery += 1
-                    num_gallery_pids.add(person_id)
+            img_paths, person_id, camera_idx = tracklet
+            if camera_idx == 0:  # Camera A
+                query.append(tracklet)
+                tracklet_query += 1
+                num_query_pids.add(person_id)
+            else:  # Cameras B and C
+                gallery.append(tracklet)
+                tracklet_gallery += 1
+                num_gallery_pids.add(person_id)
 
         num_query_pids =  len(list(num_query_pids))
         num_gallery_pids = len(list(num_gallery_pids))
